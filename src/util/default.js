23f86c419d8a0f9f546676e8114a0f57aed26b94
Rupayan Mondal <jawda09@gmail.com>
2025-01-31 02:56:40 +0530

    refactor user_handler to use URL constant for API endpoints and add getUser method

--- src/util/default.js
+++ src/util/default.js
@@ -1,11 +1,13 @@
 const defaultConfig = {
     user_details: {
-        user_email:"",
-    username:"",
-    connectionID:"",
-    isLogin:false,
-    chats:[]
+        user_email: "",
+        username: "",
+        connectionID: "",
+        isLogin: false,
+        chats: []
     }
 }
+
+export const URL = "https://chatappv1-gyatf3b2evffcxga.centralindia-01.azurewebsites.net"
 
 export default defaultConfig;
--- src/util/user_handler.js
+++ src/util/user_handler.js
@@ -1,5 +1,8 @@
+import { URL } from "./default";
+
+
 const user_handler = {
-    login_or_register: async (username, password,isLogin) => {
+    login_or_register: async (username, password, isLogin) => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
@@ -16,7 +19,32 @@ const user_handler = {
         };
 
         try {
-            const response = await fetch(`https://chatappv1-gyatf3b2evffcxga.centralindia-01.azurewebsites.net/api/Users/${isLogin?"Login":"Register"}`, requestOptions);
+            const response = await fetch(`${URL}/api/Users/${isLogin ? "Login" : "Register"}`, requestOptions);
+            const result = await response.json();
+            console.log(result);
+            return result;
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
+        }
+    },
+    getUser: async (eamil) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "email": eamil
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        try {
+            const response = await fetch(`${URL}/api/Users/GetUser"}`, requestOptions);
             const result = await response.json();
             console.log(result);
             return result;
@@ -25,7 +53,6 @@ const user_handler = {
             throw error;
         }
     }
-    
 }
 
 export default user_handler;
