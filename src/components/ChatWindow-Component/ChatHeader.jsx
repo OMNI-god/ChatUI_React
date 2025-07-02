b859b19c1f08840d64b79d9ef78aeb9955a2ce61
Rupayan Mondal <jawda09@gmail.com>
2025-07-02 14:31:05 +0530

    feat: add modal functionality to ChatHeader for displaying user profile picture

--- src/components/ChatWindow-Component/ChatHeader.jsx
+++ src/components/ChatWindow-Component/ChatHeader.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import Modal from "../ui/Modal";
+
 export default function ChatHeader({ user }) {
+  const [viewPic, setViewPic] = useState(false);
   return (
     <>
       {user && (
@@ -7,12 +11,24 @@ export default function ChatHeader({ user }) {
             src={user.profile_pic}
             alt={user.sender}
             className="w-12 h-12 object-contain rounded-full cursor-pointer"
-
+            onClick={() => setViewPic(true)}
           />
           <div className="flex justify-between px-2 min-w-0 w-full">
             <p className="font-bold">{user.sender}</p>
             <p>3 dot</p>
           </div>
+          <Modal isOpen={viewPic}>
+            <div className="m-3 flex-1 justify-center">
+              <p className="font-extrabold">{user.sender}</p>
+              <img
+                src={user.profile_pic}
+                alt={user.sender}
+                className="w-[40%] h-[1%] rounded-md"
+                onClick={() => setViewPic(true)}
+              />
+              <button className="p-2 bg-red-500 rounded-md m-2" onClick={() => setViewPic(false)}>Close</button>
+            </div>
+          </Modal>
         </nav>
       )}
     </>
--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,11 +1,26 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({isOpen, childern }) {
-  const dialog = useRef();
+export default function Modal({ isOpen, children }) {
+  const dialogRef = useRef();
 
-  return createPortal(
-    <dialog ref={dialog}>{childern}</dialog>,
-    document.getElementById("modal")
-  );
+  useEffect(() => {
+    const modal = dialogRef.current;
+    if (!modal) return;
+
+    if (isOpen) {
+      if (!modal.open) {
+        modal.showModal();
+      }
+    } else {
+      if (modal.open) {
+        modal.close();
+      }
+    }
+  }, [isOpen]);
+
+  const container = document.getElementById("modal");
+  if (!container) return null;
+
+  return createPortal(<dialog ref={dialogRef} className="w-3/6 h-4/6 rounded-md border border-gray-100 shadow-md">{children}</dialog>, container);
 }
