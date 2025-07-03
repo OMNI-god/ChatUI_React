5590ce610e8877fb55d559932cfb514d5b41d82e
Rupayan Mondal <jawda09@gmail.com>
2025-07-04 01:00:03 +0530

    feat: enhance AuthForm component with loading state and action handling

--- src/components/ui/Loading-Animations.jsx
+++ src/components/ui/Loading-Animations.jsx
@@ -1,8 +1,10 @@
 import { Circles, CirclesWithBar, LineWave, RotatingLines } from "react-loader-spinner";
 
-export default function CircleLoader(){
+export default function CircleLoader({width,height}){
     return <RotatingLines
     strokeColor="white"
     strokeWidth="2"
+    width={width}
+    height={height}
     visible={true}/>
 }
